92867ee279a8400d9f99276c1a0b60b0f9c3eff9
CHALANI23 <kumara19781030@gmail.com>
2026-02-09 01:02:37 +0530

    Add files via upload

---
+++ EvenOddNumber .java
@@ -0,0 +1,10 @@
+public class EvenOddNumber {
+    
+    public boolean findEvenOrOdd(int i) {
+        if (i % 2 == 0) {
+            return true; 
+        } else {
+            return false; 
+        }
+    }
+}
