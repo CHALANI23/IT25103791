31e859c42e741d42aa4b373b75049b69b444f534
CHALANI23 <kumara19781030@gmail.com>
2026-02-09 00:53:21 +0530

    Add files via upload

---
+++ Activity1.java
@@ -0,0 +1,14 @@
+public class Activity1 { 
+    public static void main(String[] args) { 
+         
+        int miles; 
+        int yards; 
+        double kilometers; 
+ 
+        miles = 26; 
+        yards = 385; 
+  
+        kilometers = (miles + (yards / 1760.0)) * 1.609; 
+  System.out.println("The marathon distance in kilometers is: " + kilometers); 
+    } 
+}
